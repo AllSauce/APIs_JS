f7a02ea48cfddf73bf99034376e83e422d6b6a52
ONEX8 <caroline.tisenius@live.se>
2022-05-12 15:34:50 +0200

    Update main.js

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -3,7 +3,9 @@ const pMap = L.map('pMap').setView([51.505, -0.09], 13);
 const attribution =
 '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
 
-const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-  const tiles = L.tileLayer(tileUrl, { attribution });
+const tileUrl = 'https://{s}.tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}.png';
+  const tiles = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png?api_key=a0b486d5-1bb7-4d20-a04f-a40912310009', {
+	maxZoom: 20,
+	attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+});
   tiles.addTo(pMap);
-
