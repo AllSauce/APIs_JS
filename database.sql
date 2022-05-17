3d5b2891610645be327d4f562cc62b8d530e5dbc
Grumpylama <65866382+Grumpylama@users.noreply.github.com>
2022-05-17 22:00:28 +0200

    Update database.sql

--- database.sql
+++ database.sql
@@ -14,19 +14,18 @@
 
 
 -- Dumping database structure for sysdb
+DROP DATABASE IF EXISTS `sysdb`;
 CREATE DATABASE IF NOT EXISTS `sysdb` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci */;
 USE `sysdb`;
 
 -- Dumping structure for table sysdb.comments
+DROP TABLE IF EXISTS `comments`;
 CREATE TABLE IF NOT EXISTS `comments` (
   `CommentID` int(11) NOT NULL AUTO_INCREMENT,
   `UserID` int(11) NOT NULL,
-  `Text` text COLLATE utf8_unicode_ci NOT NULL,
-  `ResponseID` int(11) DEFAULT NULL,
+  `Text` int(11) NOT NULL,
   PRIMARY KEY (`CommentID`),
   KEY `FK_comments_users` (`UserID`),
-  KEY `FK_comments_comments` (`ResponseID`),
-  CONSTRAINT `FK_comments_comments` FOREIGN KEY (`ResponseID`) REFERENCES `comments` (`CommentID`),
   CONSTRAINT `FK_comments_users` FOREIGN KEY (`UserID`) REFERENCES `users` (`UserID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
@@ -35,18 +34,16 @@ CREATE TABLE IF NOT EXISTS `comments` (
 /*!40000 ALTER TABLE `comments` ENABLE KEYS */;
 
 -- Dumping structure for table sysdb.users
+DROP TABLE IF EXISTS `users`;
 CREATE TABLE IF NOT EXISTS `users` (
   `UserID` int(11) NOT NULL AUTO_INCREMENT,
-  `Username` varchar(50) COLLATE utf8_unicode_ci NOT NULL DEFAULT '0',
+  `Username` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
   `Hash` varchar(60) COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`UserID`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
--- Dumping data for table sysdb.users: ~2 rows (approximately)
+-- Dumping data for table sysdb.users: ~0 rows (approximately)
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-REPLACE INTO `users` (`UserID`, `Username`, `Hash`) VALUES
-	(1, 'asd', '$2y$10$EioBtIe5bbAXKnipZ91i1uiDXfMSga/utTC5PjKh8sRuN52urpSme'),
-	(2, 'uer', '$2y$10$JLkT5lf.8CB/IlUqnQCB4uIuI8hn9QCGv7h0QHbjpvMfg4O18ABiO');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
