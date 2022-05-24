fd5b3a1ed125c7a1e9ea69e59810be3af6baf7aa
ONEX8 <caroline.tisenius@live.se>
2022-05-24 21:02:56 +0200

    Uppdaterad med styling

--- index.php
+++ index.php
@@ -12,38 +12,42 @@ function displayComment($activerow){
   if(check_login()){
     if($activerow['UserID'] == $_SESSION['user_id'] || $_SESSION['RoleID'] == 1){
       echo '<div class = "comment-container">
-      <div class = "comment-header">          
+      <div class = "comment-header">
         <div class = "comment-header-right">
-          <h3>'.$activerow['Username'].'</h3>
-          <p>'.$activerow['Text'].'</p>
+          <h3 id="comment">'.$activerow['Username'].'</h3>
+          <p id="comment">'.$activerow['Text'].'</p>
+
         </div>
       </div>
-      <div class = "comment-footer">          
+      <div class = "comment-footer">
         <div class = "comment-footer-right">
           <form action = "comment.php" method = "POST">
             <input type = "hidden" name = "commentid" value = "'.$activerow['CommentID'].'">
             <input type = "submit" value = "Delete">
           </form>
         </div>
+        <hr id="comment">
       </div>';
     }
     else{
       echo '<div class = "comment-container">
-      <div class = "comment-header">          
-        <div class = "comment-header-right">
-          <h3>'.$activerow['Username'].'</h3>
-          <p>'.$activerow['Text'].'</p>
-        </div>
+      <div class = "comment-header">
+
+          <h3 id="comment">Användare: '.$activerow['Username'].'</h3>
+          <p id="comment">'.$activerow['Text'].'</p>
+          <hr id="comment">
+
       </div>';
-    
+
     }
   }
   else{
     echo '<div class = "comment-container">
-      <div class = "comment-header">          
+      <div class = "comment-header">
         <div class = "comment-header-right">
-          <h3>'.$activerow['Username'].'</h3>
-          <p>'.$activerow['Text'].'</p>
+          <h3 id="comment">Användare: '.$activerow['Username'].'</h3>
+          <p id="comment">'.$activerow['Text'].'</p>
+          <hr id="comment">
         </div>
       </div>';
   }
@@ -57,23 +61,13 @@ function displayComment($activerow){
   <link rel="stylesheet" href="css/style.css">
   <script language="javascript" type="text/javascript"></script>
 
-  <!-- Load Esri Leaflet from CDN Work in progress för att ev koppla ihop API 2-->
+  <!-- Load Leaflet from CDN -->
+      <link rel="stylesheet" href="https://unpkg.com/leaflet@1.8.0/dist/leaflet.css" crossorigin="" />
+      <script src="https://unpkg.com/leaflet@1.8.0/dist/leaflet.js" crossorigin=""></script>
+
+      <!-- Load Esri Leaflet from CDN -->
       <script src="https://unpkg.com/esri-leaflet@^3.0.8/dist/esri-leaflet.js"></script>
-
-      <!-- Load Esri Leaflet Vector from CDN work in progress-->
-      <script src="https://unpkg.com/esri-leaflet-vector@3.1.1/dist/esri-leaflet-vector.js" crossorigin=""></script>
-
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-  <link
-  rel="stylesheet" href="https://unpkg.com/leaflet@1.8.0/dist/leaflet.css"
-  integrity="sha512-hoalWLoI8r4UszCkZ5kL8vayOGVae1oxXe/2A4AO6J9+580uKHDO3JdHb7NzwwzK5xr/Fs0W40kiNHxM9vyTtQ=="
-  crossorigin=""
-  />
-  <script
-  src="https://unpkg.com/leaflet@1.8.0/dist/leaflet.js"
-  integrity="sha512-BB3hKbKWOc9Ez/TAwyWxNXeoV9c1v6FIeYiBieIWkpLjauysF18NzgR1MBNBXf8/KABdlkX68nAhlwcDFLGPCQ=="
-  crossorigin=""
-  ></script>
+      <script src="https://unpkg.com/esri-leaflet-vector@^3.0.0/dist/esri-leaflet-vector.js"></script>
 
   <title>Hitta ditt utegym</title>
 </head>
@@ -86,7 +80,7 @@ function displayComment($activerow){
     <div class="header">
     <nav>
         <ul id="main-menu">
-          <li><img src="img/utegym-logo.png" id="logo"></li>
+          <li><a href="index.php"><img src="img/utegym-logo.png" id="logo"></a></li>
           <li><a href="login.php">LOGGA IN</a></li>
           <li><a href="register.php">SKAPA KONTO</a></li>
         </ul>
@@ -98,24 +92,24 @@ function displayComment($activerow){
     <div class="header">
     <nav>
         <ul id="main-menu">
-          <li><img src="img/utegym-logo.png" id="logo"></li>
+          <li><a href="index.php"><img src="img/utegym-logo.png" id="logo"></a></li>
           <li><a href="index.php">HEM</a></li>
           <li><a href="logout.php">LOGGA UT</a></li>
         </ul>
-    </nav>'; 
+    </nav>';
   }
 
   ?>
   <div class="row">
     <div class="column">
       <div class="left-column">
-        <h1><b>Lorem Ipsum</b></h1>
-        <p>Lorem ipsum </p>
+        <h1><b>Hitta ditt utegym i Uppsala</b></h1>
+        <p>Uppsala kommun tillhandahåller +20 utegym runtom Uppsala med omnej. Alla är välkomna att träna utan kostnad!</p>
+          <br>
+          <br>
         <br>
-        <br>[Sökfält kopplat till kartan - kolla vad som finns i API dokumentationen]
         <br>
-        <br>
-        <br><p class="small-body">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        <br><p class="small-body">Platsdatan för utegym tillhandahålls av <a href="https://opendata.uppsala.se/" target=”_blank”>Uppsala kommun (CC-BY)</a>.</p>
       </div>
     </div>
 
@@ -127,51 +121,64 @@ function displayComment($activerow){
       </div>
     </div>
   </div>
+<hr>
 
-  <div>
-    <form action="index.php" method = "POST">
-      <input type="text" name="search" placeholder="Sök efter kommentarer">
-      <input type="submit" id="search-button" value="Sök">
-    </form>
-  </div>
+<div class="row">
+  <div class="column">
+    <div class="left-column">
 
+      <div>
+        <form action="index.php" method = "POST">
+          <input type="text" name="search" placeholder="Sök efter kommentarer">
+          <input type="submit" id="search-button" value="Sök">
+        </form>
+      </div>
 
+<br>
 
-  <?php 
+  <?php
   if(check_login()){
     echo'
-    <div id = commenter>        
-      
-      <h3>Lämna en kommentar!</h3>
+    <div id = commenter>
+
+    <h2>Dela med dig av din upplevelse!</h2>
+    <p>Hjälp andra att hitta rätt utegym! Dela med dig av din upplevelse.</p>
+    <br>
       <form action = "comment.php" method = "POST">
         <textarea name = "comment" rows = "5" cols = "50"></textarea>
         <br>
         <input type = "submit" value = "Comment">
       </form>
-    
-    </div> 
+
+    </div>
     ';
   }
   else{
     echo '<div id = "commenter">
-    <h3>Logga in för att kommentera!</h3>
+    <h2>Hur var din upplevelse?</h2>
+    <p>Hjälp andra att hitta rätt utegym! Logga in och dela med dig av din upplevelse.</p>
     </div>';
   }
 
   ?>
+</div>
+</div>
 
+<div class="column">
+  <div class="right-column">
   <div id = comments>
+
+
+
     <?php
-      
-      
-      $sql = "SELECT * FROM comments JOIN users ON comments.UserID = users.UserID ORDER BY commentid DESC";                        
+      $sql = "SELECT * FROM comments JOIN users ON comments.UserID = users.UserID ORDER BY commentid DESC";
       $result = query_get0($con, $sql);
-      
-                          
+
+
       //Comment section
-      echo '<h1>Kommentarer</h1>';
+      echo '<h2>Kommentarer</h2>';
       while($row = $result->fetchArray()){
-        if(isset($_POST['search'])){        
+        if(isset($_POST['search'])){
           if(str_contain(strtolower($row['Text']), strtolower($_POST['search'])) || str_contain(strtolower($row['Username']), strtolower($_POST['search']))){
               displayComment($row);
           }
@@ -179,9 +186,14 @@ function displayComment($activerow){
         else{
           displayComment($row);
         }
-        
-      }            
+
+      }
     ?>
   </div>
+
+</div>
+</div>
+</div>
+
 </body>
 </html>
