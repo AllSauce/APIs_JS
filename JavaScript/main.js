95dbe1b3ed18aa093821b2058c71af52d4d4abad
ONEX8 <caroline.tisenius@live.se>
2022-05-30 23:57:31 +0200

    JS, nu enbart med iata-markers

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,34 +1,32 @@
 const apiKey = "AAPK19437b5f16dd406c8cc517fcda4e9522knv4OpcC0ofIyHNxLBI1Jm6YeBfnFIwYKo5AN1XCHhnxHgJAHKdyNSN7lrznBqDp";
     const basemapEnum = "ArcGIS:LightGray";
 
-
-
     const pMap = L.map("pMap", {
       minZoom: 2
-    }).setView([59.858,17.647], 9);
+    }).setView([59.75356929265679, 18.073459091832664], 7);
 
     L.esri.Vector.vectorBasemapLayer(basemapEnum, {
       apiKey: apiKey
     }).addTo(pMap);
 
     var icon = L.icon({
-        iconUrl: 'img/gym-icon.png',
+        iconUrl: 'img/plane-icon.png',
         iconSize: [27, 31],
         iconAnchor: [13.5, 17.5],
         popupAnchor: [0, -11]
       });
 
-     var markers = L.esri
-      .featureLayer({
-        url: "https://services2.arcgis.com/jUpNdisbWqRpMo35/ArcGIS/rest/services/Airports28062017/FeatureServer/0",
-        pointToLayer: function (geojson, latlng) {
-      return L.marker(latlng, {
-        icon: icon
-      });
-    }
-  }).addTo(pMap);
-
+  var markers = L.esri
+  .featureLayer({
+     url: "https://services2.arcgis.com/jUpNdisbWqRpMo35/ArcGIS/rest/services/Airports28062017/FeatureServer/0",
+     where:"iata_code IS NOT NULL",
+     pointToLayer: function (geojson, latlng) {
+   return L.marker(latlng, {
+     icon: icon
+   });
+ }
+}).addTo(pMap);
 
  markers.bindPopup(function (layer) {
- return L.Util.template('<p><strong>{name}</strong> located in {iso_country}</p>', layer.feature.properties);
+ return L.Util.template('<p><strong><a href=\' comments.php?airfield={iata_code} \'>{name}</a></strong> located in {iso_country}</p>', layer.feature.properties);
  });
